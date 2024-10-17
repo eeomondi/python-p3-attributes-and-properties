934158d2518cb2968fbd90c3ea374b162b72ba42
Elvis <elvis.omondi@student.moringaschool.com>
2024-10-17 12:11:59 +0300

    commit

--- lib/dog.py
+++ lib/dog.py
@@ -5,8 +5,8 @@ class Dog:
     ]
 
     def __init__(self, name="Unknown", breed="Mutt"):
-        self.name = name
-        self.breed = breed
+        self.name = name  # This will trigger the name setter
+        self.breed = breed  # This will trigger the breed setter
 
     @property
     def name(self):
@@ -18,7 +18,7 @@ class Dog:
             self._name = value
         else:
             print("Name must be string between 1 and 25 characters.")
-            self._name = "Unknown"
+            self._name = "Unknown"  # Default to "Unknown" if invalid
 
     @property
     def breed(self):
@@ -30,11 +30,10 @@ class Dog:
             self._breed = value
         else:
             print("Breed must be in list of approved breeds.")
-            self._breed = "Mutt"
+            self._breed = "Mutt"  # Default to "Mutt" if invalid
 
     def bark(self):
         print("Woof!")
 
     def sit(self):
         print("The dog is sitting.")
-
